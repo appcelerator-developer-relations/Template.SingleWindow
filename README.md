6b172069c22a860e05e233c56218a7e99a3f6749
Kevin Whinnery <kevin.whinnery@gmail.com>
2012-01-10 07:52:11 -0800

    new readme

--- README.md
+++
@@ -1,3 +0,0 @@
-# Single Window Application Template for Titanium Studio
-
-A single window application with a single view - a mostly blank canvas. Requires Titanium Mobile SDK 1.8.0+
--- Resources/app.js
+++ Resources/app.js
@@ -5,7 +5,7 @@
  * In app.js, we generally take care of a few things:
  * - Bootstrap the application with any data we need
  * - Check for dependencies like device type, platform version or network connection
- * - Require and open out top-level UI component
+ * - Require and open our top-level UI component
  *  
  */
 
